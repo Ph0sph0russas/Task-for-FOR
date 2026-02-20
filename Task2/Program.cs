cbfa928ce614b64af28d03ee51db1236f82162c9
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-02-20 19:17:36 +0800

    Первая версия программы

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int answerTaskA = 0;
+            double aForTaskA = a;
+            double sumDeposit = 0;
+            for (int i = 2; sumDeposit < b; sumDeposit = aForTaskA * 0.02)
+            {
+                answerTaskA = i;
+                i = i + 1;
+                aForTaskA = aForTaskA * 1.02;
+            }
+            Console.WriteLine(sumDeposit);
+            Console.WriteLine("Ответ под а)" + answerTaskA);
+
+            double aForTaskB = a;
+            int answerTaskB = 0;
+            for (int i = 1; aForTaskB < c; aForTaskB = aForTaskB * 1.02)
+            {
+                answerTaskB = i;
+                i = i + 1;
+
+            }
+            Console.WriteLine("Ответ под б)" + answerTaskB);
+        }
+    }
+}
